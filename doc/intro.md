bef4286e60b82015e554cff087a4c3caa6d3592b
Igor Moiseyenko <imoiseyenko93@gmail.com>
2018-10-27 21:41:19 +0200

    Initial commit.

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to okpo-charts
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ test/okpo_charts/core_test.clj
@@ -0,0 +1,7 @@
+(ns okpo-charts.core-test
+  (:require [clojure.test :refer :all]
+            [okpo-charts.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
